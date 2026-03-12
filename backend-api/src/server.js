b373f0bb6d5c35bd55ee1c01219e810362fa9315
Narith-hen <narith.hen2025@gmail.com>
2026-03-12 20:45:57 +0700

    add new service

--- backend-api/src/server.js
+++ backend-api/src/server.js
@@ -93,7 +93,7 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '1mb' }));
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 // Backward-compatible fallback:
